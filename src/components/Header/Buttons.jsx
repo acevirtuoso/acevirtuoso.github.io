4aac45d947e457105074c033b6e3d57bc0e660d9
Aly Mohamed <aly.mohamed@ucalgary.ca>
2023-11-10 18:14:01 -0700

    adding the resume to the project

--- src/components/Header/Buttons.jsx
+++ src/components/Header/Buttons.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import CV from '../../assets/CV.pdf'
+import Resume from '../../assets/Aly_Mohamed_resume.pdf'
 
 const Buttons = () => {
   return (
     <div className="headerbuttons">
-        <a id= "download-button" className='btn' href={CV} download> Download resume</a>
+        <a id= "download-button" className='btn' href={Resume} download> Download resume</a>
         <a href='mailto: aly.mohamed@ucalgary.ca' className='btn' target='_blank'>Contact Me</a>
     </div>
   )
